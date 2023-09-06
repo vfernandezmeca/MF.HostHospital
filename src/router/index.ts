05ced631f567e1865f2586bd31e759c86bc75396
Victor <vfernandezmeca@gmail.com>
2023-09-06 08:31:58 +0200

    Init proyect

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MapView from '../views/MapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
-    {
-      path: '/map',
-      name: 'map',
-      component: MapView
-    }
+ 
   ]
 })
 
